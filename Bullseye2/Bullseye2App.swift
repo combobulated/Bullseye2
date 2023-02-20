f0f5c0b3eea8c01438332cf6d927d35c612a925a
Jim Diaz <james.diaz@ieee.org>
2023-02-20 04:06:19 -0500

    Initial Commit

---
+++ Bullseye2/Bullseye2App.swift
@@ -0,0 +1,17 @@
+//
+//  Bullseye2App.swift
+//  Bullseye2
+//
+//  Created by jim on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Bullseye2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bullseye2
+//
+//  Created by jim on 2/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
